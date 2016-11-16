effb90cb4cef937cfcf2f64161a0a745d3b2a8ab
0luaP <pssfjr79@gmail.com>
2016-11-16 11:00:25 -0400

    Codigo de movimentação da câmera adicionado,e redimensionamento da câmera através do size, mudança de sprite do dinossauro

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+    public GameObject player;
+    public float min;
+    public float max;
+    //private Vector3 camera;
+	void Start () {
+	
+	}
+	
+	void Update () {
+        if (player.transform.position.x > min && player.transform.position.x < max)
+        {
+            //camera = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+            //transform.position = Vector3.Lerp(transform.position, camera);
+            transform.position = new Vector3(player.transform.position.x, 0, -10);
+        }
+    }
+}
