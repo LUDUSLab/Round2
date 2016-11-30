0ddb5cfbd0cc1e523d0ff888f9f277e2d0f9b0f2
Jansen <jansen4alcantara@gmail.com>
2016-11-30 00:36:48 -0400

    Correcao de arquivos que eu caguei durante o merge

--- Assets/Scripts/MenuBehaviourScript.cs
+++ Assets/Scripts/MenuBehaviourScript.cs
@@ -1,80 +1,52 @@
 ﻿using UnityEngine;
 using System.Collections;
+ 
+public class MenuBehaviourScript : MonoBehaviour
+{
+    Animator anim;
+ 	float escolha;
+ 	private bool dificil;
+ 	private bool centro = true;
+ 	private bool hard = false;
+ 	private bool easy = false;
 
-public class MenuBehaviourScript : MonoBehaviour {
 
+    void Start()
+    {
 
-	public GameObject start;
-	public GameObject dificuldade;
-	public GameObject visao;
-	public GameObject menu;
-	public GameObject easyer;
-	public GameObject harder;
+    }
 
-
-	private bool centro = false;
-	private bool hard = false;
-	private bool easy = false;
-	private bool primeira = true;
-	void Start () {
-		Instantiate(start, menu.transform);
-		visao.transform.position = new Vector3 (menu.transform.position.x, 0, -10);
-	
-	
-	}
-	void Update ()
-	{
-		if (primeira && Input.GetKeyDown ("Enter")) {
-			primeira = false;
-			start.transform.position = new Vector3 (0, 0, -15);
-			Instantiate (dificuldade, menu.transform);
-			centro = true;
-		}
-		if (Input.GetKeyDown ("Esc")) {
-			primeira = true;
-			start.transform.position = menu.transform.position;
-			centro = false;
-			hard = false;
-			easy = false;
-		}
-		if (centro && Input.GetKeyDown ("D")) {
-			visao.transform.position = new Vector3 (easyer.transform.position.x, 0, -10);
-			centro = false;
-			hard = true;
-		}
-		if (centro && Input.GetKeyDown ("A")) {
-			visao.transform.position = new Vector3 (harder.transform.position.x, 0, -10);
-			centro = false;
-			hard = true;
-		}
-		if (hard && Input.GetKeyDown ("A")) {
-			visao.transform.position = new Vector3 (easyer.transform.position.x, 0, -10);
-			centro = false;
-			easy = true;
-		}
-		if (hard && Input.GetKeyDown ("D")) {
-			visao.transform.position = new Vector3 (dificuldade.transform.position.x, 0, -10);
-			centro = true;
-			hard = false;
-		}
-		if (easy && Input.GetKeyDown ("D")) {
-			visao.transform.position = new Vector3 (harder.transform.position.x, 0, -10);
-			easy = false;
-			hard = true;
-		}
-		if (easy && Input.GetKeyDown ("A")) {
-			visao.transform.position = new Vector3 (dificuldade.transform.position.x, 0, -10);
-			centro = true;
-			easy = false;
-		}
-		if (easy && Input.GetKeyDown ("Enter")) {
-			//setar a vida chamando o script do player e mudando o valor da variavel
-			Application.LoadLevel ("movementjj");
-		
-		}
-		if (hard && Input.GetKeyDown ("Enter")) {
-			//setar a vida chamando o script do player e mudando o valor da variavel
-			Application.LoadLevel ("movementjj");
-		}
-  }
-}
+ 	void Update()
+ 	{
+ 		escolha = Input.GetAxis ("Horizontal");
+ 		//anim.SetBool ("Ligado", hard);
+ 		if (centro && escolha > 0) {
+ 			centro = false;
+ 			hard = true;
+ 		}
+ 		if (centro && escolha< 0) {
+ 			centro = false;
+ 			easy = true;
+ 		}
+ 		if (hard && escolha< 0) {
+ 			hard = false;
+ 			easy = true;
+ 		}
+ 		if (easy && escolha > 0) {
+ 			easy = false;
+ 			hard = true;
+ 		}
+ 		if (easy) {
+ 			if (Input.GetKey (KeyCode.KeypadEnter) || Input.GetKey ("enter")) {
+ 				dificil = false;
+ 				Application.LoadLevel ("Stage 1");
+ 			}
+ 		}
+ 		if (hard) {
+ 			if (Input.GetKey (KeyCode.KeypadEnter) || Input.GetKey ("enter")) {
+ 				dificil = true;
+ 				Application.LoadLevel ("Stage 1");
+ 			}
+ 		}
+  	}
+ }
